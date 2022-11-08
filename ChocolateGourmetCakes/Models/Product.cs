0d3e7cf12b0ee58d5d232fd7352b7fef0e461052
Jen Casto <CastoCustomCreations@gmail.com>
2022-11-08 10:45:18 -0500

    git add Add files via upload

---
+++ ChocolateGourmetCakes/Models/Product.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.Json;
+
+namespace ChocolateGourmetCakes.Models
+  {
+  public class Product
+    {
+      public int Product_id { get; set; }
+    public string Product_catagory { get; set; }
+    public string Product_title { get; set; }  
+    public string Product_description { get; set; }    
+    
+    public double Product_price { get; set; }
+    public int[] Rating { get; set; }
+    public override string ToString() => JsonSerializer.Serialize<Product>(this);
+      
+
+
+    }
+  }
